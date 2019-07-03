ee47652f450b8ef1b32b8b44343cb1ade6764fbb
Stefano <stefanopaulo47@gmail.com>
2019-07-03 08:36:19 -0300

    Set

---
+++ set.js
@@ -0,0 +1,17 @@
+// não aceita repetição/não indexada
+const times = new Set()
+times.add('Vasco')
+times.add('São Paulo').add('Palmeiras').add('Corinthias')
+times.add('Flamengo')
+times.add('Vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('vasco')) // false
+console.log(times.has('Vasco')) // true
+times.delete('Flamengo')
+console.log(times.has('Flamengo'))
+
+const nomes = ['Raquel', 'Lucas', 'Julia', 'Lucas']
+const nomesSet = new Set(nomes)
+console.log(nomesSet)
