a97160c4ee6ea711c9c6d6259878ceb47a7fb8b2
Stefano <stefanopaulo47@gmail.com>
2019-07-03 08:59:29 -0300

    Promises

--- promisses.js
+++ promisses.js
@@ -6,7 +6,7 @@ function falarDepoisDe(segundos, frase) {
     })
 }
 
-falarDepoisDe(3, 'Que legal!')
+falarDepoisDe(3, 'Que legal!?')
     .then(frase => frase.concat('?!?'))
     .then(outraFrase => console.log(outraFrase))
     .catch(e => console.log(e))
